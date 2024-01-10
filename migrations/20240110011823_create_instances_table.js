f24b77c1d2d9907fa44c8aad2697e26c138d6277
JoshDaBosh <jwanggt@gmail.com>
2024-01-09 20:26:07 -0500

    add initial instances model

---
+++ migrations/20240110011823_create_instances_table.js
@@ -0,0 +1,24 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable("instances", (t) => {
+        t.increments("id").unsigned().primary();
+        t.timestamp("created").defaultTo(knex.fn.now());
+        t.timestamp("updated").defaultTo(knex.fn.now());
+        t.integer("challenge_id").unsigned();
+        t.integer("team_id").unsigned();
+        t.text("deployment_type");
+        t.text("subdomain");
+        t.integer("port").unsigned();
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTable("challenges")
+};
--- models/instance.js
+++ models/instance.js
@@ -2,6 +2,33 @@ const Model = require("./model");
 
 class Instance extends Model {
     static tableName = "instances"
+
+    static get properties() {
+        return {
+            ...super.properties,
+            challenge_id: {
+                valid: (challenge_id) => typeof challenge_id === "number",
+                required: true  
+            },
+            team_id: {
+                valid: (team_id) => typeof team_id === "number",
+                required: true
+            },
+            deployment_type: {
+                valid: (deployment_type) =>
+                    deployment_type === "http" || deployment_type === "nc",
+                required: true
+            },
+            subdomain: {
+                valid: host => typeof host === "string",
+                required: true,
+            },
+            port: {
+                valid: port => typeof port === "number",
+                required: true
+            }
+        };
+    }
 }
 
 module.exports = Instance
